9bae9cff007aab921fd30df4da037fc31de65018
Eduardo Sanches Bocato <dubocato@gmail.com>
2020-12-29 17:15:50 -0300

    WIP: Router and ViewBuilders

--- Sources/SwiftUIRouter/Routing/Router.swift
+++ Sources/SwiftUIRouter/Routing/Router.swift
@@ -1,74 +1,84 @@
 import SwiftUI
 import Combine
 
-public protocol ViewRoute: Identifiable {
-    static var identifier: String { get }
-}
-public extension ViewRoute {
-    var id: String { Self.identifier }
-    func eraseToAnyViewRoute() -> AnyViewRoute {
-        AnyViewRoute(self)
-    }
-}
-
-public struct AnyViewRoute: ViewRoute {
-    public let metaType: Any
-    init<T: ViewRoute>(_ routeType: T.Type) {
-        metaType = routeType
-    }
-}
-
-public final class NavigationStack {
-    public internal(set) var stack: [ViewRoute] = []
-    public var current: ViewRoute? { stack.last }
-
-    public func push(_ route: ViewRoute) {
-        guard !stack.contains(
-                where: { type(of: $0).identifier == type(of: route).identifier }
-        ) else { return }
-        stack.append(route)
-    }
-
-    @discardableResult
-    public func pop() -> ViewRoute? {
-        let last = stack.popLast()
-        return last
-    }
-}
-
-public protocol ViewResolverInterface {
-    func registerViewBuilder<V: View, Environment, Route: ViewRoute>(
-        _ builder: (Environment, Route) -> V,
+public protocol ViewFactoryInterface {
+    func registerViewBuilder<Environment, Route: ViewRoute, CustomView: View>(
+        _ builder: @escaping (Environment, Route) -> CustomView,
         forRouteType routeType: Route.Type
     )
-    func viewFor<Environment>(
-        _ route: ViewRoute,
+
+    func viewFor<Route: ViewRoute, Environment>(
+        _ route: Route,
         environment: Environment
     ) -> AnyView
 }
-//
-//public final class ViewResolver
 
-public final class SwiftUIRouter {
-    // MARK: - Public Properties
+public final class ViewFactory: ViewFactoryInterface {
 
-    @Published public var presentedView: AnyView?
+    var builders: [String: AnyViewBuilder] = [:]
 
-    // MARK: - Properties
+    public func registerViewBuilder<Environment, Route: ViewRoute, CustomView: View>(
+        _ builder: @escaping (Environment, Route) -> CustomView,
+        forRouteType routeType: Route.Type
+    ) {
+        let key = routeType.identifier
+        builders[key] = AnyViewBuilder(builder)
+    }
 
-    var cancelables: Set<AnyCancellable> = .init()
-    private(set) var navigationStack: NavigationStack = .init()
-    private var cachedViews: [AnyView] = [] // do i really need this?
-
-    public init<Content: View>(@ViewBuilder initialViewBuilder: () -> Content? = { nil } ) {
-        if let initialView = initialViewBuilder() {
-            self.presentedView = AnyView(initialView)
+    public func viewFor<Route: ViewRoute, Environment>(
+        _ route: Route,
+        environment: Environment
+    ) -> AnyView {
+        let key = type(of: route).identifier
+        let typeErasedViewRoute = route.eraseToAnyViewRoute()
+        let typeErasedEnvironment = AnyEnvironment(environment)
+        return builders[key]!.erasedBuilder(typeErasedEnvironment, typeErasedViewRoute)
+    }
+}
+extension ViewFactory {
+    struct AnyViewBuilder {
+        let erasedBuilder: (AnyEnvironment, AnyViewRoute) -> AnyView
+        init<V: View, Environment, Route: ViewRoute>(
+            _ builder: @escaping (Environment, Route) -> V
+        ) {
+            erasedBuilder = { environment, route in
+                let e = environment.erasedType as! Environment
+                let r = route.eraseToAnyViewRoute() as! Route
+                let v = AnyView(builder(e, r))
+                return v
+            }
         }
     }
 
-    public func navigate(
-        toRoute route: ViewRoute,
-        style:
-    )
-
+    struct AnyEnvironment {
+        let erasedType: Any
+        init(_ erasedType: Any) {
+            self.erasedType = erasedType
+        }
+    }
 }
+
+
+//public final class SwiftUIRouter {
+//    // MARK: - Public Properties
+//
+//    @Published public var presentedView: AnyView?
+//
+//    // MARK: - Properties
+//
+//    var cancelables: Set<AnyCancellable> = .init()
+//    private(set) var navigationStack: NavigationStack = .init()
+//    private var cachedViews: [AnyView] = []
+//
+//    public init<Content: View>(@ViewBuilder initialViewBuilder: () -> Content? = { nil } ) {
+//        if let initialView = initialViewBuilder() {
+//            self.presentedView = AnyView(initialView)
+//        }
+//    }
+//
+//    public func navigate(
+//        toRoute route: ViewRoute,
+//        style:
+//    )
+//
+//}
